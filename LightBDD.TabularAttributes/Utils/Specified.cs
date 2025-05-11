347539be1b93835ec242a7c6046d196de5e0ab24
aryehcitron <aryeh.citron@newday.co.uk>
2025-05-11 23:07:41 +0100

    Fixed another serialization bug. Bump to 1.3.2

--- LightBDD.TabularAttributes/Utils/Specified.cs
+++ LightBDD.TabularAttributes/Utils/Specified.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using LightBDD.Core.ExecutionContext;
 using LightBDD.TabularAttributes.Attributes;
 
@@ -7,7 +8,11 @@ namespace LightBDD.TabularAttributes.Utils;
 
 internal static class Specified
 {
-    private static readonly JsonSerializerOptions SerializerOptions = new() { PropertyNameCaseInsensitive = true };
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        Converters = { new JsonStringEnumConverter() }
+    };
 
     public static IEnumerable<T> Inputs<T>()
         => GetValuesFromAttributes<T, HeadInAttribute, InputsAttribute>();
@@ -19,11 +24,11 @@ internal static class Specified
         where THeaders : TabularAttribute
         where TValues : TabularAttribute
     {
-        var values = GetValuesFrom<TValues>().ToArray();
+        var values = GetValuesFrom<TValues>().ToArray();        
         var headers = GetValuesFrom<THeaders>().FirstOrDefault()?.Select(x => x.ToString()).ToArray();
         headers ??= ModelReflector<T>.GetPropNames().ToArray();
 
-        var numberOfColumns = values.First().Length;
+        var numberOfColumns = values.FirstOrDefault()?.Length ?? 1;
         var numberOfRows = values.Length;
 
         var valuesByHeaderCollection = new List<T>();
@@ -33,7 +38,7 @@ internal static class Specified
             for (var columnNumber = 0; columnNumber < numberOfColumns; columnNumber++)
             {
                 var key = headers[columnNumber];
-                var value = values[rowNumber][columnNumber];
+                var value = values[rowNumber] is null ? null : values[rowNumber][columnNumber];
 
                 if (value is not null)
                 {
@@ -49,14 +54,14 @@ internal static class Specified
 
         return valuesByHeaderCollection;
 
-        string? GetJsonValue(object value) => value is string
+        string? GetJsonValue(object value) => value is string || value is Enum
             ? $"\"{value}\""
             : value is bool
                 ? value?.ToString()?.ToLower()
                 : $"{value}";
     }
 
-    private static IEnumerable<object[]> GetValuesFrom<T>() where T : TabularAttribute
+    private static IEnumerable<object?[]> GetValuesFrom<T>() where T : TabularAttribute
     {
         var method = ScenarioExecutionContext.CurrentScenario.Descriptor?.MethodInfo;
         var inputAttributes = method?.GetCustomAttributes<T>();
